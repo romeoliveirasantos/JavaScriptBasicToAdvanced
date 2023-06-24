58ca50a715e073d34b911420b88836b6118e6d5c
Romário Oliveira <rjob.oliveira@gmail.com>
2023-06-24 19:10:29 -0300

    Exemplo de operadores lógicos

--- JavaScript 1 - Lógica de programação/Operadores Lógicos/operadoresLogicos.js
+++ JavaScript 1 - Lógica de programação/Operadores Lógicos/operadoresLogicos.js
@@ -3,5 +3,25 @@
 //serve para avaliar expressões envolvendo valores booleanos
 // existem 3 operadores lógicos &&(AND) - || (OR) - ! (NOT)
 
+//&&
+idade1 = 40
+idade2 = 30
+if(idade1 === 40 && idade2 === 30){
+  console.log("Idade 1 é idêntico a " + idade1 + "\nIdade 2 é idêntico a " + idade2)
+}
+console.log( true && true)
 
+//OR
+if( idade1 === 40 || idade2 === 20){
+  console.log("Idade 1 é idêntico a " + idade1 + "\nIdade 2 não é idêntico a " + idade2)
+}
+console.log(true || false)
 
+//NOT
+
+if(!idade1 === 40){
+  console.log("Idade 1 é idêntico a " + idade1)
+}else{
+  console.log("Idade 1 não é idêntico a " + idade1)
+}
+console.log(!true)
