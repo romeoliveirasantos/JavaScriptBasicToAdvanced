de437f9456f87c4e981b9d4065ed72f8e1fba052
Romário Oliveira <rjob.oliveira@gmail.com>
2023-06-24 18:59:54 -0300

    Operadores Lógicos e Comparadores de dados

---
+++ JavaScript 1 - Lógica de programação/Comparação de dados/ComparacaoDeDados.js
@@ -0,0 +1,28 @@
+// operadores de comparação
+
+// == igual
+console.log(5 == "5")
+
+// === idêntico (igualdade estrita)
+console.log( 5 === "5")
+
+// != diferente
+console.log(5 != "5")
+
+// !=== diferente idêntico (desigualdade estrita)
+console.log(5 !== "5")
+
+// > maior
+console.log(5 > 5)
+
+// >= maior igual
+console.log(5 >= 5)
+
+// < menor
+console.log(5 < 5)
+
+// <= menor igual
+console.log(5 <= 5)
+
+
+
---
+++ JavaScript 1 - Lógica de programação/Operadores Lógicos/operadoresLogicos.js
@@ -0,0 +1,7 @@
+// operadores lógicos
+//verdadeiro ou falso
+//serve para avaliar expressões envolvendo valores booleanos
+// existem 3 operadores lógicos &&(AND) - || (OR) - ! (NOT)
+
+
+
