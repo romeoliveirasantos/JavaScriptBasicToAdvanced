e35a804e5e896f8102275b35107cbad53ffe1043
Romário Oliveira <rjob.oliveira@gmail.com>
2023-06-24 12:25:18 -0300

    Recursos Nativos do navegador

---
+++ JavaScript 1 - Lógica de programação/Recursos Nativos do navegador/recursoNativoNavegador.js
@@ -0,0 +1,42 @@
+// Recurso console
+let frutas = [
+  "Uva",
+  "Maça"
+];
+
+
+// tipos mais usados de console
+console.log(frutas);
+console.error(nome);
+console.warn(nome + frutas);
+
+
+//recurso window
+
+//exibe um alerta no browser
+//window.alert("Alerta");
+
+//exibe um popUp para inserir algo
+//window.prompt("Insira sua mensagem");
+
+//utilizando prompt para guardar alguma informação
+// let UserAge = window.prompt("Insira sua idade")
+// console.log(UserAge)
+
+//window para confirmação de alguma ação
+// let UserConfirm = window.confirm("Você confirma essa ação?")
+// console.log(UserConfirm)
+
+//lógica para fechar uma página
+// let UserConfirm = confirm("Deseja executar essa ação?")
+
+// if(UserConfirm == true){
+//   close();
+// }
+
+//window é um objeto global, não precisando ser declarado.
+
+// alert("Window.alert sem o window")
+
+
+//recurso document
