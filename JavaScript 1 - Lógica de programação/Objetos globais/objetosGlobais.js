3954ec18ec574a2e9f77c882765a9d016d6c029f
Romário Oliveira <rjob.oliveira@gmail.com>
2023-07-09 00:29:47 -0300

    Objetos globais

---
+++ JavaScript 1 - Lógica de programação/Objetos globais/objetosGlobais.js
@@ -0,0 +1,37 @@
+//objetos globais
+// são objetos padrão da linguagem que ficam disponíveis para utilizar no código
+// como por exemplo estruturas condicionais, repetiçãoes etc..
+//podem ser referenciados direto no código, mas não são estruturas, são objetos de fato.
+//não devem ser confundidos com os objetos globais como o window ou escopo global de outros ambientes
+
+// alguns exemplos de objetos globais:
+
+//Object
+//Array
+//String
+//Number
+//Data
+//Math
+
+//criando um objeto vazio com o Object
+const objeto = Object()
+
+//criando um array vazio com Array
+const lista = Array()
+
+//criando uma string vazia
+const palavra = String()
+
+//criando um número
+const numero = Number(32)
+
+//utilizando o Date
+
+const data = Date()
+
+//utilizando o Math
+const valorDePI = Math.PI
+
+
+
+
