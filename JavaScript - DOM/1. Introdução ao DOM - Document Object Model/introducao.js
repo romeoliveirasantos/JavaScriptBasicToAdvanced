7ea3d0139be99fa4e386692360ded8d002bff06a
romeoliveirasantos <rjob.oliveira@gmail.com>
2023-11-18 09:37:47 -0300

    Att

---
+++ JavaScript - DOM/1. Introdução ao DOM - Document Object Model/introducao.js
@@ -0,0 +1,12 @@
+//introdução - o que vamos aprender nesse módulo?
+
+//combinar o HTML + CSS + Javascript
+
+//criar comportamentos dinâmicos nas páginas WEB
+
+//o que é DOM e como manipulá-lo
+
+//Eventos
+
+//2 mini projetos práticos.
+
--- JavaScript 1 - Lógica de programação/Estrutura de repetição - Do While/DoWhile.js
+++ JavaScript 1 - Lógica de programação/Estrutura de repetição - Do While/DoWhile.js
@@ -8,7 +8,7 @@ do{
   alert("A velocidade do veículo é " + velocidade + " km/h")
   velocidade -= 20
 }while(velocidade > 0)
-
+  
 alert("Velocidade final: " + velocidade + " km/h")
 
 
