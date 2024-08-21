ab3747ed0e7d675e9e964cf2742fa76cc17e1c3f
romeoliveirasantos <rjob.oliveira@gmail.com>
2024-08-21 00:39:55 -0300

    feature: randomColor - ESModules

--- JavaScript-Moderno/Exercicio2-ESModules-treino/js/showUserName.js
+++ JavaScript-Moderno/Exercicio2-ESModules-treino/js/showUserName.js
@@ -1,10 +1,14 @@
 export function showUserName(){
+  const colors = ['red', 'blue', 'orange', 'green', 'purple', 'pink', 'black', 'gray', 'yellow',]
+  const randomIndex = Math.floor(Math.random() * colors.length)
+  const randomColor = colors[randomIndex]
+  
   const userName = sessionStorage.getItem('userName')  
   const mainSession = document.getElementById('mainSection')
   const span = document.createElement('span')
   span.innerText = userName
   span.id = 'spanUserName'
-  span.style.color = 'green'
+  span.style.color = randomColor
   
   const h1User = document.createElement('h1')
   h1User.innerText = `Olá, `
