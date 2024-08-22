bd54e833adf70569ab9d34e2e6faac77ed864856
romeoliveirasantos <rjob.oliveira@gmail.com>
2024-08-22 00:26:56 -0300

    NPM - node package manager

---
+++ JavaScript-Moderno/NPM-na-pratica/index.js
@@ -0,0 +1,5 @@
+const lodash = require('lodash')
+
+console.log(lodash.isArray([]))
+console.log(lodash.kebabCase('Romario Oliveira dos santos'))
+console.log(lodash.camelCase('Romário oliveira dos santos'))
